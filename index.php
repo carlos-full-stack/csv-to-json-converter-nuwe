0fc2e6da0bf34142aecd84f2a1a98c3b97e0489b
Newbiecodeweaver <carlosgilino@gmail.com>
2022-10-12 17:48:32 +0200

    function created to convert .csv into .json

--- index.php
+++ index.php
@@ -11,5 +11,39 @@
     CSV file: <input name="userfile" type="file" accept=".csv" />
     <input type="submit" value="Send File" />
 </form>
+
+<?php
+
+if (isset($_FILES['userfile']['tmp_name'])) {
+
+$fileToConvert = file_get_Contents($_FILES['userfile']['tmp_name']  , true);
+
+$fileContent = array_map("str_getcsv", explode("\n", $fileToConvert));
+
+$headers = $fileContent[0];
+
+echo csvToJson($fileContent, $headers);
+
+function csvToJson($fileContent, $headers) {
+
+$json = [];
+
+ foreach ($fileContent as $row_index => $row_data) {
+	if($row_index === 0) continue;
+
+	foreach ($row_data as $col_idx => $col_val) {
+		$label = $headers[$col_idx];
+		$json[$row_index][$label] = $col_val;
+	}
+}
+
+return json_encode($json, JSON_PRETTY_PRINT);
+
+}
+
+
+}
+
+?>
 </body>
 </html>
