e72cc3b9cf866f710b9deae5fc7ef489d06cafe1
Newbiecodeweaver <carlosgilino@gmail.com>
2022-10-12 17:35:50 +0200

    basic HTML layout created

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" fileContent="IE=edge">
+    <meta name="viewport" fileContent="width=device-width, initial-scale=1.0">
+    <title>csvToJson</title>
+</head>
+<body>
+<form enctype="multipart/form-data" method="POST">
+    CSV file: <input name="userfile" type="file" accept=".csv" />
+    <input type="submit" value="Send File" />
+</form>
+</body>
+</html>
